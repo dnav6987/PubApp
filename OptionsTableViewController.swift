7a0f01e8678033a89abf41132005519e61299534
dnav6987 <dnavarro@bowdoin.edu>
2016-05-01 16:37:09 -0400

    comment sides and options

--- OptionsTableViewController.swift
+++ OptionsTableViewController.swift
@@ -9,15 +9,17 @@
 import UIKit
 
 class OptionsTableViewController: UITableViewController {
-    var menuItem: MenuItem!
+    var menuItem: MenuItem! // the item being ordered
 
     @IBOutlet weak var descriptionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // display the name of the item being ordered
         descriptionLabel.text = menuItem.name
         
+        // if there aren't choices for them to make, automatically segue to the next step
         if menuItem.options.count <= 1 {
             dispatch_async(dispatch_get_main_queue()) {
                 [unowned self] in
@@ -36,7 +38,7 @@ class OptionsTableViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if menuItem != nil {
-            if menuItem.options.count == 0 { return 1 }
+            if menuItem.options.count == 0 { return 1 } // if there are none, still want one cell for instructions to move on
             return menuItem.options.count
         }
         return 0
@@ -44,12 +46,14 @@ class OptionsTableViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("OptionCell", forIndexPath: indexPath)
-        cell.accessibilityIdentifier = "\(indexPath.row)"
+        cell.accessibilityIdentifier = "\(indexPath.row)"   // set its identifier to be it's index for parsing the data later
 
+        // if there are none we want to give an instruction to just move on.
         if menuItem.options.count == 0 {
             cell.textLabel?.text = "No options. Press to continue to sides"
             cell.detailTextLabel?.text = ""
         } else {
+            // title = name, subtitle = price
             cell.textLabel?.text = menuItem.options[indexPath.row].description
             cell.detailTextLabel?.text = menuItem.options[indexPath.row].price.asPriceString()
         }
@@ -61,10 +65,10 @@ class OptionsTableViewController: UITableViewController {
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if let sideMenu = segue.destinationViewController as? SidesTableViewController {
-            let newMenuItem = MenuItem(otherMenuItem: menuItem)
+            let newMenuItem = MenuItem(otherMenuItem: menuItem) // so we don't modify this view's menuItem
+            // get the side using the view cell's identifier that was set above
             newMenuItem.options = [menuItem.options[Int((sender?.accessibilityIdentifier)!)!]]
             sideMenu.menuItem = newMenuItem
-            
         }
     }
 }
--- SidesTableViewController.swift
+++ SidesTableViewController.swift
@@ -6,22 +6,30 @@
 //  Copyright © 2016 Dan Navarro. All rights reserved.
 //
 
+/*
+    Display the sides that can be put on this order.
+*/
+
 import UIKit
 
 class SidesTableViewController: UITableViewController {
-    var menuItem: MenuItem!
+    var menuItem: MenuItem! // the item being ordered
 
     @IBOutlet weak var descriptionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // display the item being orderd
+        
+        // don't yet know what side or toppings are wanted to don't display them
         let menuItemWithOutSidesOrAddons = MenuItem(otherMenuItem: menuItem)
         menuItemWithOutSidesOrAddons.sides = [MenuOption]()
         menuItemWithOutSidesOrAddons.addOns = [MenuOption]()
         let menuItemDescription = menuItemWithOutSidesOrAddons.asStringAndPrice()
         descriptionLabel.text = menuItemDescription.string + "\n" + menuItemDescription.price.asPriceString()
         
+        // if there aren't choices for them to make, automatically segue to the next step
         if menuItem.sides.count <= 1 {
             dispatch_async(dispatch_get_main_queue()) {
                 [unowned self] in
@@ -40,7 +48,7 @@ class SidesTableViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if menuItem != nil {
-            if menuItem.sides.count == 0 { return 1 }
+            if menuItem.sides.count == 0 { return 1 }   // if there are none, still want one cell for instructions to move on
             return menuItem.sides.count
         }
         return 0
@@ -48,12 +56,14 @@ class SidesTableViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("SideCell", forIndexPath: indexPath)
-        cell.accessibilityIdentifier = "\(indexPath.row)"
+        cell.accessibilityIdentifier = "\(indexPath.row)"   // set its identifier to be it's index for parsing the data later
         
+        // if there are none we want to give an instruction to just move on.
         if menuItem.sides.count == 0 {
             cell.textLabel?.text = "No side options. Press to continue to sides"
             cell.detailTextLabel?.text = ""
         } else {
+            // title = name, subtitle = price
             cell.textLabel?.text = menuItem.sides[indexPath.row].description
             cell.detailTextLabel?.text = menuItem.sides[indexPath.row].price.asPriceString()
         }
@@ -63,8 +73,9 @@ class SidesTableViewController: UITableViewController {
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if let toppingMenu = segue.destinationViewController as? ToppingsTableViewController {
-            let newMenuItem = MenuItem(otherMenuItem: menuItem)
+            let newMenuItem = MenuItem(otherMenuItem: menuItem) // so we don't modify this view's menuItem
             if newMenuItem.sides.count > 1 {
+                // get the side using the view cell's identifier that was set above
                 newMenuItem.sides = [newMenuItem.sides[Int((sender?.accessibilityIdentifier)!)!]]
             }
             toppingMenu.menuItem = newMenuItem
