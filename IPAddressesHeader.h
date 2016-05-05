5a625b7085b24ac7fd516b6b33ea0900cca86be0
dnav6987 <dnavarro@bowdoin.edu>
2016-05-05 11:58:08 -0400

    only allow connections when on bowdoin wifi. need to test this on actual phone

---
+++ IPAddressesHeader.h
@@ -0,0 +1,20 @@
+//
+//  IPAddressesHeader.h
+//  PubApp
+//
+//  Created by Dan Navarro on 5/5/16.
+//  Copyright © 2016 Dan Navarro. All rights reserved.
+//
+
+/*
+    briding header for finding the devices Wifi IP address
+*/
+
+
+
+#ifndef IPAddressesHeader_h
+#define IPAddressesHeader_h
+
+#include <ifaddrs.h>
+
+#endif /* IPAddressesHeader_h */
--- NetworkConnection.swift
+++ NetworkConnection.swift
@@ -6,7 +6,7 @@
 //  Comments by Dan Navarro
 
 /*
-    This is a generic network connection class that will communicate with a python server via a TCP socket
+ This is a generic network connection class that will communicate with a python server via a TCP socket
  */
 
 import UIKit
@@ -38,30 +38,36 @@ class NetworkConnection: NSObject, NSStreamDelegate {
     var delegate: NetworkConnectionDelegate!
     
     func connect(host: String, port: Int) {
-        // set up the input and output streams with the server
-        autoreleasepool {
-        NSStream.getStreamsToHostWithName(host, port: port, inputStream: &inputStream, outputStream: &outputStream)
-        }
-        
-        if inputStream != nil && outputStream != nil {
-            // if there is an error, set the streams nil so nobody tries to connect
-            if inputStream!.streamError != nil || outputStream!.streamError != nil {
-                inputStream = nil
-                outputStream = nil
-            } else {
-                
-                // This notifies when data is sent or recieved in the stream method
-                inputStream!.delegate = self
-                outputStream!.delegate = self
-                
-                // Schedule the streams
-                inputStream!.scheduleInRunLoop(.mainRunLoop(), forMode: NSDefaultRunLoopMode)
-                outputStream!.scheduleInRunLoop(.mainRunLoop(), forMode: NSDefaultRunLoopMode)
-                
-                // Open them for communication
-                inputStream!.open()
-                outputStream!.open()
+        if onBowdoinWiFi() {
+            // set up the input and output streams with the server
+            autoreleasepool {
+                NSStream.getStreamsToHostWithName(host, port: port, inputStream: &inputStream, outputStream: &outputStream)
             }
+            
+            if inputStream != nil && outputStream != nil {
+                // if there is an error, set the streams nil so nobody tries to connect
+                if inputStream!.streamError != nil || outputStream!.streamError != nil {
+                    inputStream = nil
+                    outputStream = nil
+                } else {
+                    
+                    // This notifies when data is sent or recieved in the stream method
+                    inputStream!.delegate = self
+                    outputStream!.delegate = self
+                    
+                    // Schedule the streams
+                    inputStream!.scheduleInRunLoop(.mainRunLoop(), forMode: NSDefaultRunLoopMode)
+                    outputStream!.scheduleInRunLoop(.mainRunLoop(), forMode: NSDefaultRunLoopMode)
+                    
+                    // Open them for communication
+                    inputStream!.open()
+                    outputStream!.open()
+                }
+            }
+        } else {
+            inputStream = nil
+            outputStream = nil
+            print("Connection: not on Bowdoin Wifi")
         }
     }
     
@@ -85,11 +91,11 @@ class NetworkConnection: NSObject, NSStreamDelegate {
                     self.inputStream!.read(&buffer, maxLength: buffer.count)
                     
                     var serverMessage = String(bytes: buffer, encoding: NSUTF8StringEncoding)
-
+                    
                     serverMessage = serverMessage!.stringByReplacingOccurrencesOfString("\0", withString: "", options: NSStringCompareOptions.LiteralSearch, range: nil)
                     if serverMessage != nil && serverMessage!.characters.count >= ServerResponses.RESPONSE_CODE_LENGTH {
                         let responseCode = serverMessage!.substringToIndex(serverMessage!.startIndex.advancedBy(ServerResponses.RESPONSE_CODE_LENGTH))
-
+                        
                         // handel the message from the server
                         switch responseCode {
                         case ServerResponses.READY_FOR_PICK_UP:
@@ -147,5 +153,119 @@ class NetworkConnection: NSObject, NSStreamDelegate {
                 break
             }
         }
+    }
+    
+    // determine if the device is on bowdoin Wifi using the fact that all bowdoin IP addresses start with 139.140
+    private func onBowdoinWiFi() -> Bool {
+        let bowdoinIPSpace = "139.140"
+        
+        let RUNNING_ON_SIMULATOR = runningOnSimulator()
+        
+        if RUNNING_ON_SIMULATOR {
+            let deviceIPAddrs = getIFAddresses()
+            if deviceIPAddrs.count > 0 {
+                let deviceIPAddr = deviceIPAddrs[0]
+                if deviceIPAddr.characters.count >= bowdoinIPSpace.characters.count {
+                    if deviceIPAddr.substringToIndex(deviceIPAddr.startIndex.advancedBy(bowdoinIPSpace.characters.count)) == bowdoinIPSpace {
+                        return true
+                    }
+                }
+            }
+        } else if let deviceIPAddr = getWiFiAddress() {
+            if deviceIPAddr.characters.count >= bowdoinIPSpace.characters.count {
+                if deviceIPAddr.substringToIndex(deviceIPAddr.startIndex.advancedBy(bowdoinIPSpace.characters.count)) == bowdoinIPSpace {
+                    return true
+                }
+            }
+        }
+        
+        return false
+    }
+    
+    // Taken directly from http://stackoverflow.com/questions/30748480/swift-get-devices-ip-address
+    // Return IP address of WiFi interface (en0) as a String, or `nil`
+    // NOTE: this only works on iPhones (and maybe other iOS devices)
+    private func getWiFiAddress() -> String? {
+        var address : String?
+        
+        // Get list of all interfaces on the local machine:
+        var ifaddr : UnsafeMutablePointer<ifaddrs> = nil
+        if getifaddrs(&ifaddr) == 0 {
+            
+            // For each interface ...
+            var ptr = ifaddr
+            while ptr != nil {
+                defer { ptr = ptr.memory.ifa_next }
+                
+                let interface = ptr.memory
+                
+                // Check for IPv4 or IPv6 interface:
+                let addrFamily = interface.ifa_addr.memory.sa_family
+                if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+                    
+                    // Check interface name:
+                    if let name = String.fromCString(interface.ifa_name) where name == "en0" {
+                        
+                        // Convert interface address to a human readable string:
+                        var addr = interface.ifa_addr.memory
+                        var hostname = [CChar](count: Int(NI_MAXHOST), repeatedValue: 0)
+                        getnameinfo(&addr, socklen_t(interface.ifa_addr.memory.sa_len),
+                                    &hostname, socklen_t(hostname.count),
+                                    nil, socklen_t(0), NI_NUMERICHOST)
+                        address = String.fromCString(hostname)
+                    }
+                }
+            }
+            freeifaddrs(ifaddr)
+        }
+        
+        return address
+    }
+    
+    // Taken directly from http://stackoverflow.com/questions/30748480/swift-get-devices-ip-address
+    // return all IP addresses, this is for running on simulator and not on iPhone
+    private func getIFAddresses() -> [String] {
+        var addresses = [String]()
+        
+        // Get list of all interfaces on the local machine:
+        var ifaddr : UnsafeMutablePointer<ifaddrs> = nil
+        if getifaddrs(&ifaddr) == 0 {
+            
+            // For each interface ...
+            var ptr = ifaddr
+            while ptr != nil {
+                defer { ptr = ptr.memory.ifa_next }
+                
+                let flags = Int32(ptr.memory.ifa_flags)
+                var addr = ptr.memory.ifa_addr.memory
+                
+                // Check for running IPv4, IPv6 interfaces. Skip the loopback interface.
+                if (flags & (IFF_UP|IFF_RUNNING|IFF_LOOPBACK)) == (IFF_UP|IFF_RUNNING) {
+                    if addr.sa_family == UInt8(AF_INET) || addr.sa_family == UInt8(AF_INET6) {
+                        
+                        // Convert interface address to a human readable string:
+                        var hostname = [CChar](count: Int(NI_MAXHOST), repeatedValue: 0)
+                        if (getnameinfo(&addr, socklen_t(addr.sa_len), &hostname, socklen_t(hostname.count),
+                            nil, socklen_t(0), NI_NUMERICHOST) == 0) {
+                            if let address = String.fromCString(hostname) {
+                                addresses.append(address)
+                            }
+                        }
+                    }
+                }
+            }
+            freeifaddrs(ifaddr)
+        }
+        
+        return addresses
+    }
+    
+    // check if the device is running on a simulator or on an actual hardware device
+    private func runningOnSimulator() -> Bool {
+        var onSimulator = false
+        #if arch(i386) || arch(x86_64)
+            onSimulator = true
+        #endif
+        return onSimulator
     }
 }
